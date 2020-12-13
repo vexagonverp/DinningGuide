32ca81c6d8a78d0c2f05317ad5c9260a1bc977cc
Verp Berp <verproti80@yahoo.com.vn>
2020-12-13 11:22:38 +0700

    Fixed bugs of data not being refference correctly while attempting to end my own life due to the buggy code

--- Models/Restaurant/Restaurant.cs
+++ Models/Restaurant/Restaurant.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.Spatial;
 namespace Dinning_Guide.Models.Restaurant
 {
-
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
     public partial class Restaurant
     {
